29d6cf9f061bbd7dad2e8600c6444a0cbf4455ac
gatana <sgatana@gmail.com>
2019-02-27 11:09:38 +0300

    feat(components) refactor App component

--- src/App.spec.js
+++ src/App.spec.js
@@ -3,6 +3,8 @@ import App from './App';
 import ReactDOM from 'react-dom'
 import { configure, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
+import { Header } from './components/Header';
+import Wrapper from './components/Wrapper';
 
 // configure({adapter: new Adapter()})
 
@@ -10,6 +12,8 @@ describe('#App Component', () => {
   it('renders without crashing', () => {
     const wrapper = shallow(<App />)
     expect(wrapper.find('div')).toHaveLength(1)
+    expect(wrapper.find(Header)).toHaveLength(1)
+    expect(wrapper.find(Wrapper)).toHaveLength(1)
   })
 })
 
