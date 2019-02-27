39f7f9d7cc7a2e484022126c5b1c2daf63695463
gatana <sgatana@gmail.com>
2019-02-27 11:07:17 +0300

    feat(components) add header component

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Navbar } from 'react-bootstrap';
+
+export const Header = props => (
+  <Navbar bg="primary" variant="dark">
+    <Navbar.Brand href="#home">Randon Phone Number Generator</Navbar.Brand>
+  </Navbar>
+)
